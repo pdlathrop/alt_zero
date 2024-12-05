a2d453394eba8bf1873c2f911b05246c78a8f450
Paul Lathrop <pdlathrop@gmail.com>
2024-12-04 16:56:55 -0800

    add binary in bin folder

--- switch/test.cpp
+++ switch/test.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "switch/process_interface.h"
 
+// cp bazel-bin/switch/test bin/test
+
 int main() {
     std::cout << "Hello, World! This is a message." << std::endl; 
     return 0; 
